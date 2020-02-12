be9c150e51bb345a5a2a942de56584e9d59fcfc4
hapsoa <hapsoa@gmail.com>
2020-02-12 16:48:12 +0900

    update

--- index.ts
+++ index.ts
@@ -2,15 +2,23 @@
 import express from "express";
 import cors from "cors";
 import child_process from "child_process";
+import path = require("path");
 import { testFunction } from "./src/testPython";
+import { makeFirstStep } from "./src/makeData";
 
 const app = express();
 app.use(cors());
+
+// TODO json data를 import 하는 과정을 시작해야 한다.
+// app.use(express.static(path.join(__dirname, "data")));
+// app.set("data", path.join(__dirname, "data"));
+// const mainData = require("/mainData.json");
 
 app.post("/", (req: express.Request, res: express.Response) => {
   // testFunction();
 
   // 처음 전처리를 진행한다.
+  // const data = makeFirstStep(mainData);
 
   // python k-mode 클러스터링을 진행하고 데이터를 전달받는다.
   const spawn = child_process.spawn;
---
+++ src/makeData.ts
@@ -0,0 +1,4 @@
+export function makeFirstStep(mainData) {
+  console.log("mainData", mainData);
+  return mainData;
+}
